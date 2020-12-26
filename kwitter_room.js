fa199a9e550367831b0935b26901750c0dddb49d
krithic2008 <72926869+krithic2008@users.noreply.github.com>
2020-12-26 16:01:15 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,2 +1,30 @@
 var roomName;
 var username=localStorage.getItem("user_name")
+document.getElementById("user_name").innerHTML="Welcome " +username;
+function addRoom(){
+    roomName=document.getElementById("room_name").value;
+    firebase.database().ref("/").child(roomName).update({
+          purpose:"adding room"
+    })
+    localStorage.setItem("roomName",roomName);
+    window.location = "kwitter_page.html";
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey =
+childSnapshot.key;
+ Room_names = childKey;
+ //Start code
+ console.log("Room Name - " + Room_names);
+ row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+ document.getElementById("output").innerHTML += row;
+ //End code
+ });});}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
