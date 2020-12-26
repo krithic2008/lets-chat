bed48370925ea2be8c7f144ec169d371c8a47d2d
krithic2008 <72926869+krithic2008@users.noreply.github.com>
2020-12-26 16:19:34 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,27 @@
+var room_name=localStorage.getItem("roomName")
+var user_name=localStorage.getItem("user_name")
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) 
+{ document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot)
+ { childKey  = childSnapshot.key; childData = childSnapshot.val();
+     if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+      } });  }); }
+getData();
+
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("roomName");
+      window.location = "kwitter.html";
+}
+function send(){
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+       });
+    
+      document.getElementById("msg").value = "";
+}
